e448b445d870218d5c7a4121a772091ab839cb1e
KhyberHekmat <KhyberHekmat@users.noreply.github.com>
2024-01-26 11:34:27 +0100

    26 Janv work

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\ResetPasswordRequestFormType;
+use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -31,9 +37,41 @@ class SecurityController extends AbstractController
     }
 
     #[Route('/forget-pass', name: 'forgotten_password')]
-    public function forgottenPassword(): Response
-    {
+    public function forgottenPassword(
+        Request $request,
+        UsersRepository $usersRepository,
+        TokenGeneratorInterface $tokenGenerator,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $form = $this->createForm(ResetPasswordRequestFormType::class);
 
-        return $this->render('security/reset_password_request.html.twig');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $usersRepository->findOneByEmail($form->get('email')->getData());
+
+            //We will verify if the user exist
+            if ($user) {
+                //We will generate the token for password reset and will store it in the database
+                $token = $tokenGenerator->generateToken();
+                $user->setResetToken($token);
+                $entityManager->persist($user);
+                $entityManager->flush();
+                //Now we will generate the reset link
+                $url = $this->generateUrl('reset_pass', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+            }
+            //if the user is not exist
+            $this->addFlash('danger', 'The user is not exit');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/reset_password_request.html.twig', [
+            'requestPassForm' => $form->createView()
+        ]);
+    }
+
+    #[Route('/forget-pass/{token}', name: 'reset_pass')]
+    public function resetPass(): Response
+    {
     }
 }
